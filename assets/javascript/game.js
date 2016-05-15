507473e409752d7239c82db850bd7fc1cb6ec1a5
Lance Martin <ldmartin7777@gmail.com>
2016-05-15 10:18:07 -0500

    created hero objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  var ironMan = {
+    health: 120,
+    counterAttack: 8,
+  };
+
+  var capAmerica = {
+    health: 110,
+    counterAttack: 11,
+  };
+
+  var thor = {
+    health: 95,
+    counterAttack: 13,
+  };
+
+  var winterSoldier = {
+    health: 115,
+    counterAttack: 10,
+  };
+});
